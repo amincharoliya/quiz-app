7b0c62fd631e44aab0017a40ca48e64f83355d84
Amin Charoliya <amincharoliya@gmail.com>
2021-09-27 23:39:02 +0530

    Update 404 page UI

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,13 +1,43 @@
 import React from 'react'
-import Header from './Header';
+import styled from 'styled-components';
 
+import Header from './Header';
+import Footer from './Footer';
+
+
+const _404Page = styled.section`
+    background-color: var(--main-bg-color);
+    padding: 55px 15px;
+    min-height: calc(100vh - 132px);
+    display: flex;
+    align-items: center;
+    text-align: center;
+
+    h1 {
+        font-size: 34px;
+        font-weight: 700;
+        margin-bottom: 60px;
+    }
+
+    img {
+        max-width: 650px;
+        width: 90%;
+        max-height: 450px;
+    }
+`
 
 const NoMatch = () => {
     return (
-        <div className="wrapper">
-            <Header />
-            <h1>Seems Like you are Lost</h1>
-        </div>
+        <>
+        <Header />
+            <_404Page>
+                <div className="wrapper">
+                    <h1>Page not found!</h1>
+                    <img src='./images/404.svg' />
+                </div>
+            </_404Page>
+            <Footer />
+        </>
     )
 }
 
