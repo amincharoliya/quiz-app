3ff5575f805de1ed510ee35fb59022ff2eb2371e
Amin Charoliya <amincharoliya@gmail.com>
2021-10-09 15:25:54 +0530

    Make Avg score round

--- src/components/AverageScore.js
+++ src/components/AverageScore.js
@@ -36,7 +36,7 @@ const AverageScore = ({ data }) => {
     const avgScore = () => {
         if(data?.length) {
             const totalScore = data.map( (item) =>  item.score).reduce((a, b) => a + b)
-            return (totalScore / data.length).toFixed(2);
+            return Math.round(totalScore / data.length);
         } else {
             return 0;
         }        
